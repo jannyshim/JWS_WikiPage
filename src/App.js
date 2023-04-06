a45991bd0a0f58afe58f913acf745c7c8426f8a0
jannyshim <29jannyy@gmail.com>
2023-04-06 17:51:39 +0900

    feat: 더미데이터 생성, 헤더 생성, 라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Wikipage from "./components/Wikipage";
+import Newwiki from "./components/NewWikipage";
+import Editwiki from "./components/EditWikipage";
+
 function App() {
   return (
-    <div>
-      <h1>hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/wikipage/:id" element={<Wikipage />} />
+        <Route path="/newwiki" element={<Newwiki />} />
+        <Route path="/editwiki/:id" element={<Editwiki />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Global Knowledge Wiki Page</h1>
+    </div>
+  );
+}
